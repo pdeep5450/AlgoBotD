e276482ef52de42bd23be7f5290d8f122631b7b3
pdeep5450 <pdeep5450@gmail.com>
2025-11-16 20:35:38 +0530

    Delete app directory

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{padding:20}}>
-      <h1>AlgoBotD Website Working!</h1>
-      <p>Your Next.js Deployment is Ready.</p>
-    </div>
-  );
-}
