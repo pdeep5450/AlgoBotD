e55e5d80af8679897960733f8e4871fcf71d6e7d
pdeep5450 <pdeep5450@gmail.com>
2025-11-16 18:28:25 +0530

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{padding:20}}>
+      <h1>AlgoBotD Website Working!</h1>
+      <p>Your Next.js Deployment is Ready.</p>
+    </div>
+  );
+}
