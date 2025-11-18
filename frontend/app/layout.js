13370d0dba2bd0b6b70c787454e81a3711c726ec
pdeep5450 <pdeep5450@gmail.com>
2025-11-18 10:01:42 +0530

    Add files via upload

--- frontend/app/layout.js
+++
@@ -1,8 +0,0 @@
-export const metadata = { title: "AlgoBot", description: "Algo trading bot" };
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className="bg-gray-100">{children}</body>
-    </html>
-  );
-}
--- frontend/app/page.js
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-  return (
-    <div className="p-10 text-center text-3xl">Home Page Working</div>
-  );
-}
